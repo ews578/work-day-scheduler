36dfb8dfc4021aa53902afaae14a45f8f000a5f2
ews578 <ewsnider28@yahoo.com>
2024-01-25 07:36:27 -0600

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# work-day-scheduler
+Description
+
+The goal of this project was to build a functioning work day scheduler, one that would allow the user to save events for each hour of a typical working day. It features dynamically updated HTML and CSS powered by JavaScript and jQuery, and Dayjs library.
+
+
+There is a display in the header of the current date and time, updating every second with the current time.
+The scheduler features 9 time-blocks, starting at 9am and ending at 5pm. The color of each text-block will change color depending on whether it is in the past(grey), present(red), or future(green).
+Any text entered into the text field of an individual time-block can be saved to local storage using the provided save button. Once in local storage, it will remain displayed in that time-block upon reloading of the page.
+
+Link to deploy application https://ews578.github.io/work-day-scheduler/
+
+
+Screenshot of application in live server
+![work day scheduler](<assets/Screenshot (7).png>)
--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
   // added code to displat the current date in header
   // function currentTime() {
-    var realTime = dayjs().format("dddd, MMMM D, YYYY h:mm:ss A");
-    console.log(realTime);
-    $("#currentDay").text(realTime);
+  var realTime = dayjs().format("dddd, MMMM D, YYYY h:mm:ss A");
+  console.log(realTime);
+  $("#currentDay").text(realTime);
   // };
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -16,27 +11,27 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-    $(".saveBtn").on("click",function(){
-      var  value = $(this).siblings(".description").val()
-      console.log(value);
-      var key = $(this).parent().attr("id")
-      localStorage.setItem(key, value);
-    })
+  $(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".description").val()
+    console.log(value);
+    var key = $(this).parent().attr("id")
+    localStorage.setItem(key, value);
+  })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
-  $('.time-block').ready(function() {
+  $('.time-block').ready(function () {
     // Get the current hour
     var currentHour = dayjs().hour();
-  
+
     // Iterate through each time block element
-    $('.time-block').each(function() {
+    $('.time-block').each(function () {
       var blockHour = parseInt($(this).attr('data-hour'));
-      console.log(blockHour,currentHour)
-      
+      console.log(blockHour, currentHour)
+
       // Compare the block hour with the current hour
       if (blockHour < currentHour) {
         // Add class for past time block
@@ -50,19 +45,19 @@ $(function () {
       }
     });
   });
-    
-  
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-    $("#hour-9  .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  $("#hour-9  .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
